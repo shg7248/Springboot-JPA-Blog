8eadb67d41b2b1eabd2ac43e96864e46c124bcf2
super <super@DESKTOP-S1V8SNF>
2022-01-20 14:03:24 +0900

    중간커밋

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -2,18 +2,42 @@ package com.cos.blog.model;
 
 import java.sql.Timestamp;
 
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// ORM -> Java(다른언어) Object -> 테이블로 생성
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
 public class User {
 	
-	private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+	private int id; // 시퀀스, auto_increment
 	
-	private String username;
+	@Column(nullable = false, length = 30)
+	private String username; // 아이디
 	
+	@Column(nullable = false, length = 100) // 해쉬 (비밀번호 암호화)
 	private String password;
 	
+	@Column(nullable = false, length = 50)
 	private String email;
 	
+	@ColumnDefault("'user'")
+	private String role; // Enum을 쓰는게 좋다.
+	
+	@CreationTimestamp // 시간이 자동으로 입력
 	private Timestamp createDate;
 }
